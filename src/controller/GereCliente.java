0d195e53b9c249c496aa4ce2fc011174f79ef939
NuG <NuG@DESKTOP-M2VSPM9>
2017-03-31 17:55:54 +0100

    Muitas mudanças no projeto!

---
+++ src/controller/GereCliente.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Cliente;
+
+public class GereCliente {
+	ArrayList<Cliente> arCliente;
+	
+	public GereCliente(){
+		this.arCliente = new ArrayList<Cliente>();
+	}
+
+	public ArrayList<Cliente> getArCliente() { return arCliente; }
+	public void setArCliente(ArrayList<Cliente> arCliente) { this.arCliente = arCliente; }
+	
+	public void addCliente(String username, String primeiro_nome, String ultimo_nome, String morada, String contacto){
+		Cliente c = new Cliente(username, primeiro_nome, ultimo_nome, morada, contacto);
+		int novoID = arCliente.size();
+		c.setId(novoID);
+		arCliente.add(c);
+	}
+	
+	public Cliente procuraCliente(String username){
+		for(Cliente c : arCliente){
+			if(username.compareTo(c.getUsername()) == 0){
+				return c;
+			}
+		}
+		return null;
+	}
+}
--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,119 +1,55 @@
 package model;
 
 public class Cliente {
-	int id;
-	String  username,
-			primeiro_nome,
-			ultimo_nome,
-			morada,
-			contacto;
-	int cargo;
-	boolean deleted;
-	int numCartao;
-	
-	
-	public Cliente(int id, String username, String primeiro_nome, String ultimo_nome, String morada, String contacto,
-			int cargo, boolean deleted, int numCartao) {
+	private int 	id;
+	private String	username,
+					primeiro_nome,
+					ultimo_nome,
+					morada,
+					contacto;
+	public Cliente(String username, String primeiro_nome, String ultimo_nome, String morada, String contacto) {
 		super();
-		this.id = id;
 		this.username = username;
 		this.primeiro_nome = primeiro_nome;
 		this.ultimo_nome = ultimo_nome;
 		this.morada = morada;
 		this.contacto = contacto;
-		this.cargo = cargo;
-		this.deleted = deleted;
-		this.numCartao = numCartao;
 	}
-
-
 	public int getId() {
 		return id;
 	}
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
 	public String getUsername() {
 		return username;
 	}
-
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-
 	public String getPrimeiro_nome() {
 		return primeiro_nome;
 	}
-
-
 	public void setPrimeiro_nome(String primeiro_nome) {
 		this.primeiro_nome = primeiro_nome;
 	}
-
-
 	public String getUltimo_nome() {
 		return ultimo_nome;
 	}
-
-
 	public void setUltimo_nome(String ultimo_nome) {
 		this.ultimo_nome = ultimo_nome;
 	}
-
-
 	public String getMorada() {
 		return morada;
 	}
-
-
 	public void setMorada(String morada) {
 		this.morada = morada;
 	}
-
-
 	public String getContacto() {
 		return contacto;
 	}
-
-
 	public void setContacto(String contacto) {
 		this.contacto = contacto;
-	}
-
-
-	public int getCargo() {
-		return cargo;
-	}
-
-
-	public void setCargo(int cargo) {
-		this.cargo = cargo;
-	}
-
-
-	public boolean isDeleted() {
-		return deleted;
-	}
-
-
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
-	}
-
-
-	public int getNumCartao() {
-		return numCartao;
-	}
-
-
-	public void setNumCartao(int numCartao) {
-		this.numCartao = numCartao;
 	}
 	
 }
